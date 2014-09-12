d54eea25f7439ce70a40977d924187cc1953726a
Blair Walsh <walshbj2@student.op.ac.nz>
2014-09-12 06:05:19 +0000

    uptodate splatter-client

--- splatter-client/app.js
+++ splatter-client/app.js
@@ -1,72 +1,146 @@
 (function() { // We use this anonymous function to create a closure.
 
 	var app = angular.module('splatter-web', ['ngResource']);
+	//window.showfeed = "";
 
-	app.factory('User', function($resource) {
-		return $resource('http://walsh.sqrawler.com/api/users/:id.json', {id: '@id'},
-			{'update': {method:'PUT'}} );
-			follow: {method:'POST', url:'http://walsh.sqrawler.com/api/users/follows/'},
-addsplatt: {method:'POST', url:'http://walsh.sqrawler.com/api/splatts/'},
-unfollow: {method:'DELETE', url:'http://walsh.sqrawler.com/api/users/follows/:id/:follows_id'}})
+	app.factory('User', function($resource, $http) 
+	{
+		return $resource('http://walsh.sqrawler.com/api/users/:id.json',{},{
+		update: {method:'PUT'},
+		follow: {method:'POST', url:'http://walsh.sqrawler.com/api/users/follows/'},
+		addsplatt: {method:'POST', url:'http://walsh.sqrawler.com/api/splatts/'},
+		unfollow: {method:'DELETE', url:'http://walsh.sqrawler.com/api/users/follows/:id/:follows_id'}})
 	});
 	
-	app.factory('SplattsFeed', function($resource)
-{
-return $resource('http://walsh.sqrawler.com/api/users/splatts-feed/:id.json',{},{
-getsplatts: {method:'GET',url:'http://walsh.sqrawler.com/api/users/splatts/:id.json', isArray:true}});
-});
+	app.factory('SplattsFeed', function($resource) 
+	{
+		return $resource('http://walsh.sqrawler.com/api/users/splatts-feed/:id.json',{},{
+		getsplatts: {method:'GET',url:'http://walsh.sqrawler.com/api/users/splatts/:id.json', isArray:true}});
+	});
 
-	app.controller('UserController', function(User) {
-		this.data = {};
+	app.controller('UserController', function(User, SplattsFeed) 
+	{
 		this.ulist = User.query();
-		this.getUser = function(i) {
-		return User.get({id: i});
-	});
-
-	this.login = function() {
-		this.loggedin_user = this.getUser(this.data.id);
-		this.data = {};
-	};
-
-	this.updateUser = function() {
-		this.loggedin_user.name = this.data.name;
-		this.loggedin_user.email = this.data.email;
-		this.loggedin_user.blurb = this.data.blurb;
-		this.loggedin_user.$update();
-		this.data = {};
 	});
 	
-	app.controller("AddUserController", function(User){
+	app.controller("ShowSplattsController", function(User, SplattsFeed) 
+		{
 		this.data = {};
-		this.createUser = function(user) {
-		var dataName = this.data.cname;
-		var dataMail = this.data.cemail;
-		var dataPassword = this.data.cpassword;
-		user = new User({name: dataName, email: dataMail, password: dataPassword});
-		//user.$save();
-		User.save(user,user);
-		document.getElementById('prompt').innerHTML = "User Added";
-		//this.user = User.save({name: dataName, email: dataMail, password: dataPassword});
-		this.data = {}; // clears the form
+		this.updateFeed = function(user) 
+		{			
+			SplattsFeed.getsplatts({id: this.data.id})
+			.$promise.then(
+        //success
+			function( value ){user.feed.splatt = value});
+			this.data = {} //clears the form}
+			//Set the user and feed to be the user we just got
+			
+		}
+	});	
+
+	
+	app.controller("DeleteUserController", function(User) 
+	{
+		this.data = {};
+		this.deleteUser = function(user) 
+		{	
+			deleteId = this.data.id;			
+			User.delete({id: deleteId});
+			this.data = {} //clears the form
+		}
+	});	
+
+	app.controller("UpdateUserController", function(User) 
+	{	
+		this.data = {};
+		this.updateUser = function(user) 
+		{	
+			postData = {"user":{
+				"name": this.data.name, 
+  				"blurb": this.data.blurb 
+			}}
+			
+				
+			User.update({id: user.u.id},postData);
+			this.data = {} //clears the form
+		}
 	}
-	});
-
-	this.createUser = function() {
-		u = new User();
-		u.name = this.data.cname;
-		u.email = this.data.cemail;
-		u.password = this.data.cpassword;
-		u.blurb = this.data.cblurb;
-		u.$save();
+	);
+	
+	app.controller("AddSplattController", function(User) 
+	{	
 		this.data = {};
-	};
-
-	this.delUser = function() {
-		//alert("Deleting "+ this.data.delid);
-		User.delete({id: this.data.delid});
-	};
+		this.addSplatt = function(user) 
+		{	
+			postData = {"splatt":{
+				"body": this.data.splatt, 
+  				"user_id": user.u.id 
+			}}
+			
+				
+			User.addsplatt({},postData);
+			this.data = {} //clears the form
+		}
+	}
+	);
 	
-	});
-
+		app.controller("FollowUserController", function(User) 
+	{	
+		this.data = {};
+		this.followUser = function(user) 
+		{	
+			followUserData = {"id": user.u.id,"follows_id": this.data.follows}
+			User.follow({},followUserData);
+			this.data = {} //clears the form
+		}
+	}
+	);
 	
+		app.controller("UnfollowUserController", function(User) 
+	{	
+		this.data = {};
+		this.unfollowUser = function(user) 
+		{	
+			User.unfollow({id: user.u.id,follows_id: this.data.follows},{});
+			this.data = {} //clears the form
+		}
+	}
+	);
+	
+	
+	// add your form controller below
+	app.controller("UpdateFormController", function(User, SplattsFeed) 
+	{
+		this.data = {};
+		this.updateUser = function(user) 
+		{			
+			//Set the user from the api
+			newUser = User.get({id: this.data.name});
+			//Set the news feed from the api
+			newFeed = SplattsFeed.get({id: this.data.name});
+			//Set the user and feed to be the user we just got
+			user.u = newUser;
+			user.feed = newFeed;
+			this.data = {} //clears the form
+		}
+	});	
+	
+	app.controller("NewUserFormController", function(User, $http) 
+	{
+		this.data = {};
+		this.newUser = function(user) 
+		{	
+			postData = {"user":{
+  				"email": this.data.email, 
+  				"name": this.data.name, 
+				"password": this.data.password,
+  				"blurb": this.data.blurb 
+			}}
+			
+			
+			 User.save(postData);
+			this.data = {} //clears the form
+		}
+	});	
+    // add your form controller above
 })();
