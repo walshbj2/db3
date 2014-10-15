9e27f6b25757998193f999cb36b72b3328778891
Blair Walsh <walshbj2@student.op.ac.nz>
2014-10-15 01:40:39 +0000

    8.2 finished

--- splatter/app/controllers/users_controller.rb
+++ splatter/app/controllers/users_controller.rb
@@ -1,120 +1,88 @@
 class UsersController < ApplicationController
-	 before_filter :cors_preflight_check
-	 after_filter :cors_set_access_control_headers
-
-  # GET /users
-  # GET /users.json
-  def index
-    @users = User.all
-
-    render json: @users
-  end
-
-  # GET /users/1
-  # GET /users/1.json
-  def show
-    @user = User.find(params[:id])
-
-    render json: @user
-  end
-
-  # POST /users
-  # POST /users.json
-  def create
-    @user = User.new user_params(params[:user])
-
-    if @user.save
-      render json: @user, status: :created, location: @user
-    else
-      render json: @user.errors, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /users/1
-  # PATCH/PUT /users/1.json
-  def update
-    @user = User.find(params[:id])
-
-    if @user.update user_params(params[:user])
-      head :no_content
-    else
-      render json: @user.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /users/1
-  # DELETE /users/1.json
-  def destroy
-    @user = User.find(params[:id])
-    @user.destroy
-
-    head :no_content
-  end
- 
-  # GET /users/follows/[:id]
-  def show_follows
-    @user = User.find(params[:id])
-
-    render json: @user.follows
-  end 
-
-  # GET /users/followers/[:id]
-  def show_followers
-    @user = User.find(params[:id])
-
-    render json: @user.followers
-  end
-
-  # POST /users/follows
- def add_follows
-	#params[:id] is user who follows 
-	#params[:follows_id] is user to be followed
-	
-	#make follower
-	@follower = User.find(params[:id])
-	
-	#make followed
-	@followed = User.find(params[:follows_id])
-	
-	if 
-		@follower.follows << @followed
-		head :no_content
-	else
-		render json: @follower.errors, status: :unprocessable_entity
-	end
+before_filter :cors_preflight_check
+after_filter :cors_set_access_control_headers
+# GET /users
+# GET /users.json
+def index
+@users = User.all
+render json: @users
 end
-
-  # DELETE /users/follows/1/2
-  def delete_follows
-   @follower = User.find(params[:id])
-	@followed = User.find(params[:follows_id])
-
-	@follower.follows.delete(@followed)
-		head :no_content
-		
-	end	
-
-def splatts
-  @user = User.find(params[:id])
-render json: @user.splatts
-end
-
 # GET /users/splatts-feed/1
 def splatts_feed
 @feed = Splatt.find_by_sql("SELECT splatts.body, splatts.user_id, splatts.id, splatts.created_at FROM splatts JOIN follows ON follows.followed_id=splatts.user_id WHERE follows.follower_id=#{params[:id]} ORDER BY created_at DESC")
 render json: @feed
 end
-
-private
-
-def user_params(params)
-  params.permit(:email, :password, :name, :blurb)
+# GET /users/splatts/[:id]
+def splatts
+@user = User.find(params[:id])
+render json: @user.splatts
 end
-
+# GET /users/follows/[:id]
+def show_follows
+@user = User.find(params[:id])
+render json: @user.follows
+end
+# POST /users/follows
+def add_follows
+@user = User.find(params[:id])
+@follows = User.find(params[:follows_id])
+if @user.follows << @follows
+head :no_content
+else
+render json: @user.errors, status: :unprocessable_entity
+end
+end
+# DELETE /users/follows/1/2
+def delete_follows
+@user = User.find(params[:id])
+@follows = User.find(params[:follows_id])
+if @user.follows.delete(@follows)
+head :no_content
+else
+render json: @user.errors, status: :unprocessable_entity
+end
+end
+# GET /users/1
+# GET /users/1.json
+def show
+@user = User.find(params[:id])
+render json: @user
+end
+# POST /users
+# POST /users.json
+def create
+@user = User.new(user_params(params))
+if @user.save
+render json: @user, status: :created, location: @user
+else
+render json: @user.errors, status: :unprocessable_entity
+end
+end
+# PATCH/PUT /users/1
+# PATCH/PUT /users/1.json
+def update
+@user = User.find(params[:id])
+if @user.update(user_params(params))
+head :no_content
+else
+render json: @user.errors, status: :unprocessable_entity
+end
+end
+# DELETE /users/1
+# DELETE /users/1.json
+def destroy
+@user = User.find(params[:id])
+@user.destroy
+head :no_content
+end
+private
+def user_params(params)
+logger.info "params: #{params}"
+params.permit(:id, :name, :email, :password, :blurb)
+end
 def set_headers
 headers['Access-Control-Allow-Origin'] = '*'
 end
-
 # For all responses in this controller, return the CORS access control headers.
 def cors_set_access_control_headers
 headers['Access-Control-Allow-Origin'] = '*'
@@ -131,5 +99,4 @@ headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
 headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version'
 headers['Access-Control-Max-Age'] = '1728000'
 end
-
 end
--- splatter/app/models/user.rb
+++ splatter/app/models/user.rb
@@ -1,31 +1,7 @@
-class User < ActiveRecord::Base
-has_many :splatts
-
-has_and_belongs_to_many :follows,
-    class_name: "User",
-    join_table: :follows,
-    foreign_key: :follower_id,
-    association_foreign_key: :followed_id
-
-has_and_belongs_to_many :followed_by,
-	class_name: "User",
-	join_table: :follows,
-	foreign_key: :followed_id,
-	association_foreign_key: :follower_id
-
-# To require a value in the name field
-	validates :name, presence: true
-
-# To enforce uniqueness for email addresses
-	validates :email, uniqueness: {case_sensitive: false}
-
-# To enforce length requirement on passwords min of 8 charaters
-	validates :password, length: {minimum: 8}, if: :strong?
-
-	def strong?
-		password =~ /.*\d+.*/ && \
-		password =~ /.*[a-z]+.*/ && \
-		password =~ /.*[A-Z].*/
-	end
+class User
+  include Mongoid::Document
+  field :name, type: String
+  field :email, type: String
+  field :password, type: String
+  field :blurb, type: String
 end
-
